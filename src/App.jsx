5474b58556feefce4bbdce5c3dc78f8e069fd75e
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-08-25 22:46:32 -0500

    Added the first exercise "Create a div with the class large and the id largeDiv with the text Hi"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+
 function App() {
-  return "Hello World";
+  return (
+    <h1 id="5" tabIndex>
+      Hello World <span>Bye</span>
+    </h1>
+  );
 }
 
 export default App;
+
+// Exercise #1: Create a div with the class large and the id largeDiv with the text Hi
+
+function App() {
+  return (
+    <div id="largeDiv" className="large">
+      Hi
+    </div>
+  );
+}
