47393242fabc557cfea07dcce2d677a0e525877e
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-08-25 22:52:23 -0500

    Added the next exercise to remove the text and add a label

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,26 @@ function App() {
 export default App;
 
 // Exercise #1: Create a div with the class large and the id largeDiv with the text Hi
-
+/*
 function App() {
   return (
     <div id="largeDiv" className="large">
       Hi
     </div>
   );
-}
+} 
+*/
+
+// Remove the text in hi
+// Add a label with the for prop set to inputId and any text
+// Ad an input with the id InputId, type number, and value of 3 as a number
+/*
+function App() {
+  return (
+    <div id="largeDiv" className="large">
+    <label htmlFor="inputId">SDfsfa</label>
+    <input id="inputId" type="number" defaultValue={3} />
+    </div>
+  );
+} 
+*/
