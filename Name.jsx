bd589da287ce65231aadd4ccd0ff6f70ca0740cf
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-08-26 21:29:04 -0500

    Added instructor name method

--- Name.jsx
+++ Name.jsx
@@ -1,3 +1,15 @@
+// Instructors method
+import React from "react";
+
+export class Name extends React.Component {
+  render() {
+    return <h1>Kathy Xiong</h1>;
+  }
+}
+
+// My method
+/* 
 export function Name() {
   return <h1>Hello my name is Kathy</h1>;
 }
+*/
