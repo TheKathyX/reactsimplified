0a9d1f42749e92e3e0c78ad20126ce5a4a83e09d
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-09-19 20:06:10 -0500

    Created ClassComponent.jsx

--- FunctionComponent.jsx
+++ FunctionComponent.jsx
@@ -1,1 +1,19 @@
-export function FunctionComponent() {}
+import { useState } from "react";
+
+export function FunctionComponent() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <br />
+      <br />
+      <button onClick={() => setAge((currentAge) => currentAge - 1)}>-</button>
+      <button onClick={() => setAge((currentAge) => currentAge + 1)}>+</button>
+      <br />
+      <br />
+      My name is {name} and I am {age} years old.
+    </div>
+  );
+}
