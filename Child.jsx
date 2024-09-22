38f23cda27e7862668e1aa1dc3b98a63ce97bffe
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-09-21 23:27:46 -0500

    Updated Child function

--- Child.jsx
+++ Child.jsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 
 export function Child() {
   const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
   console.log("render child");
 
@@ -10,7 +11,13 @@ export function Child() {
     <div className="child">
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <br />
-      My name is {name}.
+      <br />
+      <button onClick={() => setAge((currentAge) => currentAge - 1)}>-</button>
+      {age}
+      <button onClick={() => setAge((currentAge) => currentAge + 1)}>+</button>
+      <br />
+      <br />
+      My name is {name} and I am {age} years old.
     </div>
   );
 }
