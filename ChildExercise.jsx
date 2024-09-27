824776a9d4c9412ea5ffc42a963a8d9d447f5639
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-09-26 21:05:38 -0500

    updated the childExercise with mentors methods

--- ChildExercise.jsx
+++ ChildExercise.jsx
@@ -4,6 +4,46 @@ export function ChildExercise() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
 
+  // the text render each time the component re-renders
+  useEffect(() => {
+    console.log("Re-render");
+  });
+
+  // the text Hi when the component mounts
+  useEffect(() => {
+    console.log("Hi");
+  }, []);
+
+  // the texxt My name is {name} and I am {age} years old whenever the name or age changes
+  useEffect(() => {
+    console.log(`My name is ${name} and I am ${age} years old`);
+  }, [name, age]);
+
+  // update the document title to be equal to name whenever the name changes
+  useEffect(() => {
+    document.title = name;
+  }, [name]);
+
+  // the text Bye when the component unmounts
+  useEffect(() => {
+    console.log("Hi");
+
+    return () => {
+      console.log("Bye");
+    };
+  }, []);
+{
+  // Create a timeout that console.log's the text My name is {name} only after there has been a 1 second delay since the last time the name was changed
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`my name is ${name}`);
+    }, 1000)
+
+    return () => {
+        clearTimeout(timeout)
+    }
+  }, [name])
+
   return (
     <div>
       <input
