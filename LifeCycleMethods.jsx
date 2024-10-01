56ba03fe4bc8046af7afa3d8888905cb25200763
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-09-30 20:32:51 -0500

    Added component Mount functions in LifeCycleMethods.jsx

--- LifeCycleMethods.jsx
+++ LifeCycleMethods.jsx
@@ -8,6 +8,28 @@ export class Child extends React.Component {
       name: "",
       age: 0,
     };
+
+    this.handleDocumentClick = () => {
+      console.log(this.state.name);
+    };
+  }
+
+  componentDidMount() {
+    console.log("mount");
+  }
+
+  componentWillMount() {
+    console.log("Unmount");
+    document.removeEventListener("click", this.handleDocumentClick);
+  }
+
+  // only gets called on update
+  // also takes snapshot but it is almost never used
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.name !== this.state.name) {
+      document.removeEventListener("click", this.handleDocumentClick);
+      document.addEventListener("click", this.handleDocumentClick);
+    }
   }
 
   render() {
