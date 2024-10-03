456fe095e125feb94602b8d3c4219b82094d7860
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-10-02 23:20:15 -0500

    updated StrictMode

--- StrictMode.jsx
+++ StrictMode.jsx
@@ -1,20 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export function Child() {
-    const [age, setAge] = useState(0)
-    const [name, setName] = useState("")
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
 
+  useEffect(() => {
+    console.log("Mount");
 
-    useEffect(() => {
-        console.log("Mount")
+    return () => {
+      console.log("Unmount");
+    };
+  }, []);
 
-        return () => {
-            console.log("Unmount")
-        }
-    }, [])
-
-    return (
-        
-    )
-
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={() => setAge((a) => a - 1)}>-</button>
+    </div>
+  );
 }
