48b2097e05c7ca5f1919d4445a3975c3d6dd1fc1
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-10-24 23:05:43 -0500

    Added a ujseEffect in UserList Project

--- after/src/App.jsx
+++ after/src/App.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then(setUsers);
+  });
+
   return null;
 }
 
