a232408293afa477327a64699a71bcd572f545d4
Kathy Xiong <kathy.kxiong16@gmail.com>
2024-10-25 10:33:57 -0500

    Created a inline way to add a loading state in App

--- after/src/App.jsx
+++ after/src/App.jsx
@@ -1,22 +1,41 @@
 import { useEffect } from "react";
+import { User } from "./Users";
 
 function App() {
+  // inline way to add a loading state
+  const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    setIsLoading(true);
+
+    const controller = new AbortController();
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: constroller.signal,
+    })
       .then((res) => res.json())
-      .then(setUsers);
-  });
+      .then(setUsers)
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
       <h1>User List</h1>
-      <ul>
-        {users.map((user) => {
-          return <User name={user.name} />;
-        })}
-      </ul>
+      {isLoading ? (
+        <h2>Loading..</h2>
+      ) : (
+        <ul>
+          {users.map((user) => {
+            return <User key={user.id} name={user.name} />;
+          })}
+        </ul>
+      )}
     </>
   );
 }
